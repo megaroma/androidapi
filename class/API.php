91938313c1c628797a3d0f558e024ab9f984e22c
MegaSpy <darkromanovich@gmail.com>
2015-02-24 17:02:20 -0500

    fix

--- class/API.php
+++ class/API.php
@@ -7,6 +7,7 @@ class API {
 		if(($api_key != '') && ($api_key == $conf['api_key']) && ($action != '') && (in_array($action, $actions))) {
 			$method = 'apicall'.ucfirst($action);
 			$res = APICalls::$method();
+			print_r($res);
 			echo  json_encode ($res, JSON_HEX_QUOT);
 			exit;
 		}
