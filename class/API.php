f695c9bb8606f5879ceb908559808326f5ad7bd0
MegaSpy <darkromanovich@gmail.com>
2015-02-24 17:52:27 -0500

    fix

--- class/API.php
+++ class/API.php
@@ -7,51 +7,11 @@ class API {
 		if(($api_key != '') && ($api_key == $conf['api_key']) && ($action != '') && (in_array($action, $actions))) {
 			$method = 'apicall'.ucfirst($action);
 			$res = APICalls::$method();
-			//echo  json_encode ($res, JSON_HEX_QUOT);
-			echo self::toJSON($res);
+			echo  json_encode ($res, JSON_HEX_QUOT);
+			echo "<br><br>". json_last_error() ;
 			exit;
 		}
 	}
 
 
-	public static function toJSON($o) {
-	switch (gettype($o)) {
-		case 'NULL':
-			return 'null';
-		case 'integer':
-		case 'double':
-			return strval($o);
-		case 'string':
-			return '"' . addslashes($o) . '"';
-		case 'boolean':
-			return $o ? 'true' : 'false';
-		case 'object':
-			$o = (array) $o;
-		case 'array':
-			$foundKeys = false;
-
-			foreach ($o as $k => $v) {
-				if (!is_numeric($k)) {
-					$foundKeys = true;
-					break;
-				}
-			}
-
-			$result = array();
-
-			if ($foundKeys) {
-				foreach ($o as $k => $v) {
-					$result []= toJSON($k) . ':' . toJSON($v);
-				}
-
-				return '{' . implode(',', $result) . '}';
-			} else {
-				foreach ($o as $k => $v) {
-					$result []= toJSON($v);
-				}
-
-				return '[' . implode(',', $result) . ']';
-			}
-	}
-	}
 }
